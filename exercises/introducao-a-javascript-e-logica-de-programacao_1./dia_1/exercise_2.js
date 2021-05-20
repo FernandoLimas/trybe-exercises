e862d2ac5ba14fbb41d29f7caa8e01d2d160b99a
FernandoLimas <nando_filipe@hotmail.com>
2021-05-20 09:50:28 -0300

    exercicio de 1-5

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1./dia_1/exercise_2.js
@@ -0,0 +1,9 @@
+let numberOne= 5;
+let numberTwo=10;
+
+if(numberOne > numberTwo){
+  console.log("Number one is the bigger")
+}
+else{
+  console.log("Number two is the bigger")
+}
---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1./dia_1/exercise_3.js
@@ -0,0 +1,13 @@
+let numberOne=8;
+let numberTwo=5;
+let numberThree=1;
+
+if(numberOne>numberTwo && numberOne>numberThree){
+  console.log("Number One is the bigger")
+}
+else if(numberOne<numberTwo && numberTwo>numberThree){
+  console.log("Number Two is the bigger")
+}
+else{
+  console.log("Number Three is the bigger")
+}
