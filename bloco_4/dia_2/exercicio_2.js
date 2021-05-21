5c9782ce7af40f9714f529b13db82fc6d9dd978d
FernandoLimas <nando_filipe@hotmail.com>
2021-05-20 22:46:06 -0300

    exercicios de 1-6

---
+++ bloco_4/dia_2/exercicio_2.js
@@ -0,0 +1,10 @@
+//Somar todos os números dentro do array
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma=0;
+
+for(let i=0; i<numbers.length; i +=1){
+  soma += numbers[i]
+}
+
+console.log(soma);
