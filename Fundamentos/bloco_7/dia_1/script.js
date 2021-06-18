2efd828815a1c76d414416279fd7d49c3534370d
Fernando <nando_filipe@hotmail.com>
2021-06-17 22:29:08 -0300

    exercise-1

--- Fundamentos/bloco_7/dia_1/script.js
+++ Fundamentos/bloco_7/dia_1/script.js
@@ -12,6 +12,19 @@
 
 testingScope(true); */
 
+/* const testingScope = (escopo) => (escopo === true) ? `Não devo ser utilizada fora do meu escopo (if)` : `Não devo ser utilizada fora meu escopo (else)`;
+console.log(testingScope(false)); */
+
 //Ultlizando o código acima, mas agora com arrow functions e template literals
-const testingScope = (escopo) => (escopo === true) ? `Não devo ser utilizada fora do meu escopo (if)` : `Não devo ser utilizada fora meu escopo (else)`;
-console.log(testingScope(false));
+const testingScope = escopo => { 
+  if (escopo === true) { 
+    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+    console.log(ifScope);
+  } else {
+    const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    console.log(elseScope);
+  }
+}
+
+testingScope(true);
